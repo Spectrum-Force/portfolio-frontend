5101dd01119b9ced9f174bd05bf71e76921e7c97
Harika <ruth.agbozo.93@gmail.com>
2024-07-10 16:04:58 +0000

    login and signup pages

--- src/pages/auth/logIn.jsx
+++ src/pages/auth/logIn.jsx
@@ -2,7 +2,48 @@
 
 const LogIn = () => {
     return (
-      <div>LogIn</div>
+      <div className="grid grid-cols-2 shadow-lg mx-10 rounded-xl ">
+      
+      <form onSubmit={LogIn} >
+       <div>
+       <h1>SIGN IN</h1>
+       <p>use your account</p>
+       <label
+             htmlFor="email"
+             className="block text-[#ef4e18] font-bold mb-1 ml-4"
+           >
+             Email
+           </label>
+           <input
+             type="text"
+             id="name"
+             className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+           />
+           <label
+             htmlFor="password"
+             className="block text-[#ef4e18] font-bold mb-1 ml-4"
+           >
+             Password
+           </label>
+           <input
+             type="text"
+             id="name"
+             className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+           />
+       </div>
+       <link rel="stylesheet" href="<p>Forgot your password?</p>" />
+       <div className="mb-8">
+           <button type="submit" className="h-10 w-40 px-3 py-2 bg-orange-400 text-white border-2 rounded-3xl hover:bg-[#e7c75b]"
+           >Sign In</button>
+         </div>
+      </form>
+      <div className="bg-[#FF8DBB] rounded-xl">
+        <h1> Hello, Friend!</h1>
+        <p>Enter your personal details to start your journey with us</p>
+        <button type="submit" className=" text-white mb-8 h-10 w-40 px-3 py-2 border-2 rounded-3xl hover:bg-[#e7c75b]"
+        >SIGN UP</button>
+      </div>
+     </div>
     )
   }
   
--- src/pages/auth/signUp.jsx
+++ src/pages/auth/signUp.jsx
@@ -1,9 +1,114 @@
 
 
 const SignUp = () => {
-    return (
-      <div>SignUp</div>
-    )
-  }
-  
-  export default SignUp
+  return (
+    <div className="flex flex-col  bg-[#EDFF4E]">
+
+      <div className="text-center font-bold text-3xl">
+      <h1>Sign Up</h1>
+      <p>use your account</p>
+      </div>
+
+      <form onSubmit={SignUp} className="mx-80">
+        <div>
+          <label
+            htmlFor="firstName"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            First name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="lastName"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Last name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="otherNames"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Other names
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="email"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+
+            Email
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="password"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Password
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="confirmPassword"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="userName"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="true"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+          <label
+            htmlFor="termsAndConditions"
+            className="block text-[#FF9C60] font-bold mb-1 ml-4"
+          >
+            Terms and Conditions
+          </label>
+          <input
+            type="boolean"
+            id="name"
+            className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+          />
+        </div>
+
+        <div>
+          <button type="submit" className=" mt-5 h-10 w-40 px-3 py-2 bg-[#FF9C60] border-2 rounded-3xl hover:bg-[#e7d7e9]"
+          >Sign Up</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
