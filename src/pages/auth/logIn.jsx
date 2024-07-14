e94cf19f30621335c4d9865f0d6f999278555f42
Harika <ruth.agbozo.93@gmail.com>
2024-07-14 21:25:05 +0000

    init login and signup pages

--- src/pages/auth/logIn.jsx
+++ src/pages/auth/logIn.jsx
@@ -1,50 +1,80 @@
-
+import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
 
 const LogIn = () => {
-    return (
-      <div className="grid grid-cols-2 shadow-lg mx-10 rounded-xl ">
-      
-      <form onSubmit={LogIn} >
-       <div>
-       <h1>SIGN IN</h1>
-       <p>use your account</p>
-       <label
-             htmlFor="email"
-             className="block text-[#ef4e18] font-bold mb-1 ml-4"
-           >
-             Email
-           </label>
-           <input
-             type="text"
-             id="name"
-             className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
-           />
-           <label
-             htmlFor="password"
-             className="block text-[#ef4e18] font-bold mb-1 ml-4"
-           >
-             Password
-           </label>
-           <input
-             type="text"
-             id="name"
-             className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
-           />
-       </div>
-       <link rel="stylesheet" href="<p>Forgot your password?</p>" />
-       <div className="mb-8">
-           <button type="submit" className="h-10 w-40 px-3 py-2 bg-orange-400 text-white border-2 rounded-3xl hover:bg-[#e7c75b]"
-           >Sign In</button>
-         </div>
-      </form>
-      <div className="bg-[#FF8DBB] rounded-xl">
-        <h1> Hello, Friend!</h1>
-        <p>Enter your personal details to start your journey with us</p>
-        <button type="submit" className=" text-white mb-8 h-10 w-40 px-3 py-2 border-2 rounded-3xl hover:bg-[#e7c75b]"
-        >SIGN UP</button>
-      </div>
-     </div>
-    )
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   }
-  
-  export default LogIn
+  return (
+
+    <div className="h-[800px] flex items-center justify-center " >
+      <div className="flex flex-row shadow-lg rounded-xl items-center justify-center">
+        <div>
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-[#59BEF1] rounded-xl flex place-content-center text-xl font-bold">
+          <div className="mx-28">
+            <div className="m-9">
+            <h1>SIGN IN</h1>
+            <p>use your account</p>
+            </div>
+            
+            <label
+              htmlFor="email"
+              className="block text-black mb-1 ml-4"
+            >
+              Email/Username
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg "
+              {
+                ...register("email", { required: "email is not provided" })
+                }
+            />
+            {errors.email && (<p className="text-red-500">{errors.email.message}</p>)}
+
+            <label
+              htmlFor="password"
+              className="block text-black mb-1 ml-4"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="h-10 w-full px-2 py-1 border-black border-2 rounded-lg"
+              {
+                ...register("password", { required: "provide password" })
+                }
+            />
+            {errors.password && (<p className="text-red-500">{errors.password.message}</p>)}
+             <div className=" m-10 ml-16">
+            <button type="submit" className="h-10 w-40 px-3 py-2 bg-[#9CA3AF] text-black border-2 rounded-3xl hover:bg-[#e5e1e6]"
+            >Sign In</button>
+          </div>
+          </div>
+        </form>
+
+        </div>
+
+        <div className="bg-[#9CA3AF] h-[390px] rounded-xl w-[500px] flex items-center justify-center">
+          <div className=" text-xl fontFamily-['arial']">
+            <h1> Hello, Friend!</h1>
+            <p>Enter your personal details <br />to start your journey with us</p>
+            <p>Don't have an account?</p>
+            <Link to="/signup" className="underline font-bold">
+              SIGN UP 
+            </Link>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default LogIn
